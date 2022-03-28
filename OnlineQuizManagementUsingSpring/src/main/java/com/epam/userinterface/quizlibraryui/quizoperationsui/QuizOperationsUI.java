5be0398be96fecdf75f5e503adef588011029057
Anupama_Jha <Anupama_Jha@epam.com>
2022-03-28 21:41:18 +0530

    Final Submission 1

--- OnlineQuizManagementUsingSpring/src/main/java/com/epam/userinterface/quizlibraryui/quizoperationsui/QuizOperationsUI.java
+++
@@ -1,7 +0,0 @@
-package com.epam.userinterface.quizlibraryui.quizoperationsui;
-
-import com.epam.services.QuizService;
-
-public interface QuizOperationsUI {
-    void perform(QuizService service);
-}
